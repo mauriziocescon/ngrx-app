18d695512c2e64397245512ebdf96cc33e4b2ebd
mau <maurizio.cescon@gmail.com>
2017-12-19 18:06:53 +0100

    code cleaning

--- src/app/dynamic-form/services/list.service.ts
+++ src/app/dynamic-form/services/list.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
 import { Observable } from "rxjs/Observable";
-import "rxjs/operator/map";
+import "rxjs/add/operator/map";
 import "rxjs/add/operator/catch";
 
 import { Block, BlockType } from "../models";
