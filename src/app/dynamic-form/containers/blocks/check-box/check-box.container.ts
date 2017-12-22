41190339588798de5e2c76d8b53562733b9980c7
mau <maurizio.cescon@gmail.com>
2017-12-22 18:58:55 +0100

    addition of modal example

--- src/app/dynamic-form/containers/blocks/check-box/check-box.container.ts
+++ src/app/dynamic-form/containers/blocks/check-box/check-box.container.ts
@@ -1,9 +1,16 @@
-import { Component, ChangeDetectionStrategy, Input } from "@angular/core";
+import { Component, ChangeDetectionStrategy, Input, OnInit, OnDestroy } from "@angular/core";
 import { Store } from "@ngrx/store";
 
 import * as fromDynamicForm from "../../../reducers";
 import * as checkBox from "../../../actions/blocks/check-box.actions";
 import { CheckBoxBlock } from "../../../models";
+
+import * as fromRoot from "../../../../reducers";
+import * as modalAlertsActions from "../../../../core/actions/modal-alert.actions";
+import * as modalConfirmersActions from "../../../../core/actions/modal-confirmer.actions";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/filter";
+import { ModalAlert, ModalConfirmer } from "../../../../core/models";
 
 @Component({
   selector: "ct-check-box",
@@ -14,10 +21,55 @@ import { CheckBoxBlock } from "../../../models";
       (valueDidChange)="valueDidChange($event)">
     </cp-check-box>`,
 })
-export class CheckBoxContainerComponent {
+export class CheckBoxContainerComponent implements OnInit, OnDestroy {
   @Input() block: CheckBoxBlock;
 
-  constructor(private store: Store<fromDynamicForm.State>) {
+  modalConfirmerResult: Observable<{ [id: string]: boolean }>;
+  modalConfirmerResultSubscription: any;
+  modalConfirmerId = "1";
+
+  constructor(private store: Store<fromRoot.State>) {
+    this.modalConfirmerResult = store.select(fromRoot.getModalConfirmerResults);
+  }
+
+  ngOnInit(): void {
+    if (this.block.id === 0) {
+
+      setTimeout(() => {
+        this.subscribeToModalConfirmerResult();
+        const modalConfirmer: ModalConfirmer = {
+          id: this.modalConfirmerId,
+          title: "Confirmer",
+          message: "Are you sure about that?",
+          yesButtonLabel: "Yes",
+          noButtonLabel: "No",
+        };
+        this.store.dispatch(new modalConfirmersActions.ShowModalConfirmer({modal: modalConfirmer}));
+      }, 0);
+    }
+  }
+
+  subscribeToModalConfirmerResult(): void {
+    if (this.modalConfirmerResultSubscription) {
+      this.modalConfirmerResultSubscription.unsubscribe();
+    }
+
+    this.modalConfirmerResultSubscription = this.modalConfirmerResult
+      .subscribe((modalConfirmerResult: { [id: string]: boolean }) => {
+        if (modalConfirmerResult[this.modalConfirmerId] === true) {
+
+          console.log(`modalConfirmerResultSubscription: ${JSON.stringify(modalConfirmerResult)}`);
+
+          const modalAlert: ModalAlert = {
+            id: "1",
+            title: "Alert",
+            message: "Message",
+            buttonLabel: "Ok"
+          };
+          this.store.dispatch(new modalAlertsActions.ShowModalAlert({modal: modalAlert}));
+          this.modalConfirmerResultSubscription.unsubscribe();
+        }
+      });
   }
 
   valueDidChange(value: boolean): void {
@@ -31,5 +83,11 @@ export class CheckBoxContainerComponent {
       }
     };
     this.store.dispatch(new checkBox.ValueDidChange(block));
+  }
+
+  ngOnDestroy(): void {
+    if (this.modalConfirmerResultSubscription) {
+      this.modalConfirmerResultSubscription.unsubscribe();
+    }
   }
 }
