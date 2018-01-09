e56768e0d1c556bb31a0efc34165f6b8264f5049
mau <maurizio.cescon@gmail.com>
2018-01-09 17:48:53 +0100

    minor changes

---
+++ src/app/shared/directives/validation.directive.spec.ts
@@ -0,0 +1,8 @@
+import { BlockValidationDirective } from "./validation.directive";
+
+describe("BlockValidationDirective", () => {
+  it("should create an instance", () => {
+    const directive = new BlockValidationDirective();
+    expect(directive).toBeTruthy();
+  });
+});
