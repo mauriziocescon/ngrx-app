581e5937d041f63bdd3ed87f978ed66ffe8fe136
mau <maurizio.cescon@gmail.com>
2018-01-22 17:09:19 +0100

    store fixes

--- src/app/custom/custom-blocks/custom-blocks.module.ts
+++ src/app/custom/custom-blocks/custom-blocks.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+import { EffectsModule } from "@ngrx/effects";
+
+import { BlockUtilsService } from "../../base/dynamic-form/services";
 
 import { SharedModule } from "../../shared/shared.module";
 
+import { EFFECTS } from "./effects";
+import { reducers } from "./reducers";
 import {
   CustomBlockUtilsService,
   TextInputConfirmerService,
@@ -14,11 +20,29 @@ import {
   TextInputConfirmerContainerComponent,
 } from "./containers";
 
-import { BlockUtilsService } from "../../base/dynamic-form/services";
-
 @NgModule({
   imports: [
     SharedModule,
+
+    /**
+     * StoreModule.forFeature is used for composing state
+     * from feature modules. These modules can be loaded
+     * eagerly or lazily and will be dynamically added to
+     * the existing state.
+     */
+    StoreModule.forFeature("customBlocks", reducers),
+
+    /**
+     * Effects.forFeature is used to register effects
+     * from feature modules. Effects can be loaded
+     * eagerly or lazily and will be started immediately.
+     *
+     * All Effects will only be instantiated once regardless of
+     * whether they are registered once or multiple times.
+     */
+    EffectsModule.forFeature([
+      ...EFFECTS,
+    ]),
   ],
   declarations: [
     ...COMPONENTS,
