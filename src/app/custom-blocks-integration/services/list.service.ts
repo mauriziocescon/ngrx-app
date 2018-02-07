711c29ac2aa437d0a3629dd8a7edb830d902d642
mau <maurizio.cescon@gmail.com>
2018-02-07 19:01:16 +0100

    list effects

--- src/app/custom-blocks-integration/services/list.service.ts
+++ src/app/custom-blocks-integration/services/list.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 import { HttpClient } from "@angular/common/http";
 import { Store } from "@ngrx/store";
 
@@ -6,7 +7,7 @@ import { Observable } from "rxjs/Observable";
 
 import { AppConstantsService } from "../../core/core.module";
 
-import { BlockListService } from "../../dynamic-blocks-list/dynamic-blocks-list.module";
+import { Block, BlockListService } from "../../dynamic-blocks-list/dynamic-blocks-list.module";
 
 import { fromB1 } from "../../b1";
 
@@ -18,13 +19,25 @@ import { Modules } from "../models";
 export class CustomBlockListService extends BlockListService {
 
   constructor(protected http: HttpClient,
+              protected route: ActivatedRoute,
               protected store: Store<any>,
               protected appConstants: AppConstantsService) {
     super(
       http,
+      route,
       store,
       appConstants,
     );
+  }
+
+  getAllEditBlocksSelector(module: string, instance: string, step: string): Observable<Block[]> {
+    if (module === Modules.b1) {
+      return this.store.select(fromB1.getAllEditBlocksState);
+    } else if (module === Modules.b2) {
+      return this.store.select(fromB2.getAllEditBlocksState);
+    } else {
+      super.getAllEditBlocksSelector(module, instance, step);
+    }
   }
 
   getValiditySelector(module: string, instance: string, step: string): Observable<boolean> {
