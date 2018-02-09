3cc3f8c4778615446fe00ea3e918c3841afffe00
mau <maurizio.cescon@gmail.com>
2018-02-09 17:01:01 +0100

    minor changes

--- src/app/dynamic-blocks-list/effects/sync.effects.ts
+++ src/app/dynamic-blocks-list/effects/sync.effects.ts
@@ -1,53 +1,28 @@
 import { Injectable } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
 import { Action } from "@ngrx/store";
 import { Effect, Actions } from "@ngrx/effects";
 
 import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/do";
 import "rxjs/add/operator/switchMap";
 import "rxjs/add/operator/withLatestFrom";
 
 import { UpdateBlocks } from "../actions/list.actions";
 import { SyncActionTypes } from "../actions/sync.actions";
 
-import { BlockListService } from "../services";
+import { BlocksSyncService } from "../services";
 
 @Injectable()
 export class SyncEffects {
 
   constructor(protected actions$: Actions,
-              protected route: ActivatedRoute,
-              protected blocksList: BlockListService,) {
+              protected syncService: BlocksSyncService) {
   }
 
   @Effect() synchronization$: Observable<Action> = this.actions$
     .ofType(SyncActionTypes.REQUIRE_SYNC)
-    .withLatestFrom(this.helperFunction())
+    .withLatestFrom(this.syncService.helperFunction())
     .switchMap(([payload, blocks]) => {
-      return [new UpdateBlocks(this.payloadFunction(blocks))];
+      return [new UpdateBlocks(this.syncService.payloadFunction(blocks))];
     });
-
-  helperFunction(): any {
-    const params = this.getUpdateBlocksInstanceSelector();
-    return this.blocksList.getAllEditBlocksSelector(params.module, params.instance, params.step);
-  }
-
-  payloadFunction(blocks): any {
-    return {
-      ...this.getUpdateBlocksInstanceSelector(),
-      blocks: blocks,
-    };
-  }
-
-  getUpdateBlocksInstanceSelector(): { module: string, instance: string, step: string } {
-    const module = this.route.snapshot.paramMap.get("module");
-    const instance = this.route.snapshot.paramMap.get("instance");
-    const step = this.route.snapshot.paramMap.get("step");
-
-    return {
-      module: module,
-      instance: instance,
-      step: step,
-    };
-  }
 }
