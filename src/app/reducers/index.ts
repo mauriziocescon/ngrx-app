2c46ea708b6a92cf7a65fd17ab7ded20c3a40abf
mau <maurizio.cescon@gmail.com>
2018-02-14 11:20:12 +0100

    minor changes

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -37,8 +37,8 @@ export const reducerProvider = [
 // console.log all actions
 export function logger(reducer: ActionReducer<State>): ActionReducer<State> {
   return function (state: State, action: any): State {
-    console.log("state", state);
-    console.log("action", action);
+    // console.log("state", state);
+    // console.log("action", action);
 
     return reducer(state, action);
   };
