c1bf5687ceccfded66082fc23a1ead971bf8d8d8
mau <maurizio.cescon@gmail.com>
2018-02-14 11:45:34 +0100

    minor changes

--- src/app/b-integration/custom-blocks-integration/tokens/index.ts
+++
@@ -1,3 +0,0 @@
-import { InjectionToken } from "@angular/core";
-
-export const HOOKS_TOKEN = new InjectionToken<any>("HooksService");
---
+++ src/app/tokens/index.ts
@@ -0,0 +1,4 @@
+import { InjectionToken } from "@angular/core";
+
+export const B_BLOCK_HOOKS_TOKEN = new InjectionToken<any>("BBlocksHooksService");
+export const B_BLOCK_UTILS_TOKEN = new InjectionToken<any>("BBlocksUtilsService");
