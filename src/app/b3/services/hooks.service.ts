103e7ab10cf3986c052c26221ec383cf5e65489a
mau <maurizio.cescon@gmail.com>
2018-02-21 13:19:45 +0100

    minor changes

--- src/app/b3/services/hooks.service.ts
+++ src/app/b3/services/hooks.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from "@angular/core";
 
+import { IBlockHooks } from "../../instance-detail/instance-detail.module";
+
 import * as setOfRules from "../rules";
 
 @Injectable()
-export class B3BlockHooksService {
+export class B3BlockHooksService implements IBlockHooks {
+  key: string;
 
   constructor() {
+    this.key = "b3";
   }
 
   subscribeAll(hooks: any): void {
-
+    // do nothing
   }
 
   unsubscribeAll(): void {
-
+    // do nothing
   }
 
   getSetOfHooks(name: string): any {
