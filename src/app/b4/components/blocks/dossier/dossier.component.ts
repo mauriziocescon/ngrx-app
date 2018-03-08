de3be14002b40bd8d611ee9d0501e96464c54eac
mau <maurizio.cescon@gmail.com>
2018-03-08 18:12:38 +0100

    minor changes

--- src/app/b4/components/blocks/dossier/dossier.component.ts
+++ src/app/b4/components/blocks/dossier/dossier.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output, EventEmitter, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
 
 import { NGXLogger } from "ngx-logger";
 
@@ -9,13 +10,28 @@ import { DossierBlock } from "../../../models";
   templateUrl: "./dossier.component.html",
   styleUrls: ["./dossier.component.scss"]
 })
-export class DossierComponent {
+export class DossierComponent implements OnInit {
   @Input() block: DossierBlock;
   @Input() loading: boolean;
   @Output() valueDidChange: EventEmitter<void>;
 
-  constructor(protected logger: NGXLogger) {
+  dossierForm: FormGroup;
+
+  protected textInput1Section1Control: FormControl;
+  protected textInput2Section1Control: FormControl;
+  protected checkBox1Section1Control: FormControl;
+
+  protected textInput1Section2Control: FormControl;
+
+  constructor(protected formBuilder: FormBuilder,
+              protected logger: NGXLogger) {
     this.valueDidChange = new EventEmitter<void>();
+  }
+
+  ngOnInit(): void {
+    this.dossierForm = this.formBuilder.group({
+      textInput1: this.block.section1.label1
+    });
   }
 
   get isLoading(): boolean {
--- src/app/b4/models/blocks/dossier.model.ts
+++ src/app/b4/models/blocks/dossier.model.ts
@@ -1,8 +1,30 @@
 import { Block } from "../../../instance-detail/instance-detail.module";
 
 export interface DossierBlock extends Block {
+  section1: DossierSection1Block;
+  section2: DossierSection2Block;
   valid: boolean;
   hooks: DossierBlockHooks;
+}
+
+export interface DossierSection1Block {
+  sectionLabel: string;
+
+  label1: string;
+  value1?: string;
+
+  label2: string;
+  value2?: boolean;
+
+  label3: string;
+  value3?: boolean;
+}
+
+export interface DossierSection2Block {
+  sectionLabel: string;
+
+  label1: string;
+  value1: string;
 }
 
 interface DossierBlockHooks {
