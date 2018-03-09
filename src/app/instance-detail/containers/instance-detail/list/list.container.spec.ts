29f19e55d28f9add9f1a09a5ab0a287c9b5083cb
mau <maurizio.cescon@gmail.com>
2018-03-09 11:00:13 +0100

    minor changes

--- src/app/instance-detail/containers/instance-detail/list/list.container.spec.ts
+++ src/app/instance-detail/containers/instance-detail/list/list.container.spec.ts
@@ -22,7 +22,7 @@ export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, "assets/i18n/", ".json");
 }
 
-describe("ListComponent", () => {
+describe("ListContainerComponent", () => {
   let component: ListContainerComponent;
   let fixture: ComponentFixture<ListContainerComponent>;
 
