1d530d2246e92597074ba2b36edabbd531b1b8f0
mau <maurizio.cescon@gmail.com>
2018-03-12 16:14:12 +0100

    minor changes

--- mock-server/block-creator.js
+++ mock-server/block-creator.js
@@ -133,32 +133,36 @@ const getDatePicker = function(id) {
 };
 
 const getDossier = function(id) {
+  const section1Required1 = faker.random.boolean();
+  const section1Required2 = faker.random.boolean();
+  const section1Required3 = faker.random.boolean();
+  const section2Required1 = faker.random.boolean();
 
   let dossier = {
     id: id,
     type: "dossier",
     order: parseInt(id),
     section1: {
-      sectionLabel: "",
+      sectionLabel: "COMPONENT.DOSSIER.SECTION1_LABEL",
 
-      label1: "",
+      label1: "COMPONENT.DOSSIER.SECTION1_VALUE1_LABEL",
       value1: "",
-      required1: true,
+      required1: section1Required1,
 
-      label2: "",
+      label2: "COMPONENT.DOSSIER.SECTION1_VALUE2_LABEL",
       value2: "",
-      required2: true,
+      required2: section1Required2,
 
-      label3: "",
+      label3: "COMPONENT.DOSSIER.SECTION1_VALUE3_LABEL",
       value3: true,
-      required3: true
+      required3: section1Required3
     },
     section2: {
-      sectionLabel: "",
+      sectionLabel: "COMPONENT.DOSSIER.SECTION2_LABEL",
 
-      label1: "",
+      label1: "COMPONENT.DOSSIER.SECTION2_VALUE1_LABEL",
       value1: "",
-      required1: true
+      required1: section2Required1
     },
     valid: true,
     hooks: {
