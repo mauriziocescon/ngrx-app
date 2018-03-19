aab50e473da947927c97d4421260bc94c00976fe
mau <maurizio.cescon@gmail.com>
2018-03-19 12:57:49 +0100

    bug fixes

--- mock-server/utils.js
+++ mock-server/utils.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
 
 // delete db
-exports.deleteDb = function(path, callback) {
-  fs.unlink(path, callback);
+exports.deleteDb = (path, callback) => {
+  if (fs.existsSync(path)) {
+    fs.unlink(path, callback);
+    return;
+  }
+  callback();
 };
 
 // save db
-exports.saveDb = function(path, data, callback) {
+exports.saveDb = (path, data, callback) => {
   fs.writeFile(path, JSON.stringify(data, null, 2), callback);
 };
