2cbfa372b98cbe3a0788679c5485d2d586cb5f6c
mau <maurizio.cescon@gmail.com>
2018-03-26 13:29:36 +0200

    minor changes

--- src/app/core/services/utilities.service.ts
+++ src/app/core/services/utilities.service.ts
@@ -1,10 +1,26 @@
 import { Injectable } from '@angular/core';
 
+import * as parseLinkHeader from 'parse-link-header';
+
 import { AppConstantsService } from './app-constants.service';
 
 @Injectable()
 export class UtilitiesService {
 
   constructor(protected appConstants: AppConstantsService) {
+  }
+
+  /**
+   * Parse link property
+   * inside headers
+   *
+   * @param headers
+   */
+  parseLinkHeaders(headers: any): any {
+    if (headers && headers('link') && headers('link').length === 0) {
+      throw new Error('parseLinkHeaders: link must be defined');
+    }
+
+    return parseLinkHeader(headers('link'));
   }
 }
