89625e17c4230731fe1bc437ce8cbf00fb1fd760
Maurizio <Maurizio.CESCON@ext.ec.europa.eu>
2018-08-01 17:48:21 +0200

    minor changes

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -1,17 +1,17 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
-module.exports = function (config) {
+module.exports = function(config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('@angular-devkit/build-angular/plugins/karma'),
       require('karma-chrome-launcher'),
       require('karma-coverage-istanbul-reporter'),
       require('karma-jasmine'),
       require('karma-jasmine-html-reporter'),
       require('karma-phantomjs-launcher'),
-      require('@angular-devkit/build-angular/plugins/karma'),
     ],
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
