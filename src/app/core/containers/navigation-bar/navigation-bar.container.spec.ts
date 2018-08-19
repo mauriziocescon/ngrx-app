b9b3fef7666f2ba59893a69efd0954ec3016d8e2
Maurizio Cescon <maurizio.cescon@gmail.com>
2018-08-19 09:54:29 +0200

    chore(tests): code cleaning

--- src/app/core/containers/navigation-bar/navigation-bar.container.spec.ts
+++ src/app/core/containers/navigation-bar/navigation-bar.container.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
 
 import { APP_BASE_HREF } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
